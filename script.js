335d48ba51ea9d4e09386764808583d87ab8998c
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-02-19 19:54:14 -0300

    update

--- script.js
+++ script.js
@@ -63,6 +63,7 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 const displayMovements = function (movements) {
   containerMovements.innerHTML = '';
+
   movements.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposito' : 'saque';
     const html = `
