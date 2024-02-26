5cc7f953bd1709578a02043174f6c05245764582
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-02-26 18:31:13 -0300

    estudo sobre reduce

--- script.js
+++ script.js
@@ -89,7 +89,10 @@ const createUsernames = function (accs) {
   });
 };
 createUsernames(accounts);
-console.log(accounts);
+
+const calPrintBalance = function(movements){
+  const balance = 
+}
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -128,4 +131,13 @@ console.log(movementsDescriptions);
 
 const saques = movements.filter(mov => mov < 0);
 console.log(saques);
+
+
+console.log(movements);
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(balance);
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
 */
